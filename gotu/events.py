0f8094cb2e7716d7ad63b303f206c848adb91eff
Johnny Gill <swfiua@gmail.com>
2021-07-02 15:50:54 -0400

    axioms? of space time

---
+++ gotu/events.py
@@ -0,0 +1,37 @@
+"""Space-time
+
+Colin recommends readers take a look at Appendix A, for an
+introduction to the basic concepts of realtivity.
+
+At the start of chapter 2 important facts and concepts from the
+appendix are enumerated.
+
+1. An event is a point in space-time.   A place and a time, if you like.
+
+2. Causality.  Events A and B are causally related if it is possible
+for a signal, travelling at or below the speed of light, to pass from
+A to B.
+
+3. Observers and world lines.  An observer or world-line, is a path in
+space time with each point pre-ceding later points.
+
+4. Relativity of space and time.  Space and time are relative concepts
+which depend on the observer.
+
+5. Constancy of the speed of light.  The speed of light (measured in a
+vacuum) is the same for all observers.
+
+6. Intertial frames and observers.  Frames in which Newton's laws hold
+to a first order and obervers are stationary in such frames.  Inertial
+frames exist and to first order the geometry of an inertial fram is
+the same as Minkowski space.
+
+
+7. The laws of physics are unchanged by uniform linear motion.
+
+8. Einstein's general equivalence principle.  All systems of
+referenece are equivalent with respect to the formulation of the
+fundamental laws of physics.
+
+
+"""
