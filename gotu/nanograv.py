96ec068631abd892c75c7651cc8d6e68169e7624
Johnny Gill <swfiua@gmail.com>
2023-06-30 17:57:33 -0400

    initial thoughts on nanograv

---
+++ gotu/nanograv.py
@@ -0,0 +1,97 @@
+"""Pulsar timing array detects ultra-low frequency gravitational waves
+
+
+The news on 29 June 2023 very much lived up to the hype.
+
+Observational astronomy is going through an incredible period.
+
+An observatory tracking the timing of 68 pulsars over 15 years has
+detected gravitational waves with wavelenths of years -- that's the
+distance light travels in a year.
+
+Since the waves travel at the speed of light, it's also the time it
+takes for one wavelength, hence the frequency is in nanohertz.
+
+They have detected a wobble in space time of the order of 10m per
+light year.
+
+It is an incredible collaboration.  It was even necessary to improve
+our estimates of the movement of Jupiter, using observations from a
+Jupiter space-probe, in order to know the position of the earth
+relative to the solar system's centre of mass.
+
+Very interestingly the wave detected is approximately twice the amplitude they were expecting to find, given their models.
+
+These models assume we are seeing waves from the mergers of black
+holes at the centre of galaxies.
+
+This observation comes at a time when astrophysicists are struggling
+to explain how galaxies got so large, so soon after the big bang.
+
+Professor Colin Rourke suggests that the Big Bang never happened. He
+proposes *de Sitter Space* as a natural model for the universe.
+
+*de Sitter Space* is much misunderstood.
+
+The big bang model has precisely half the matter in the universe.
+
+The expanding universe we see is balanced by a contracting flow too.
+
+When this matter is added back in, bingo the amplitude of the wave
+will be twice what the teams modelling predicted!
+
+The observations will also be able to build a map over time showing
+how the amplitude of waves differs in different parts of the sky.
+
+I predict that this map will match the Cosmic Microwave Background.
+
+I expect teams are already looking at this.  If it is found it would
+indicate that both the waves and the CMB have the same source.
+
+This will be taken as confirmation of the big bang theory and a
+fantasy period of merging galaxies, that we do not see.
+
+What we are seeing is the sum of the rotations of all black holes in
+our visible universe.
+
+As Professor Rourke suggests, these black holes drag the surrounding
+spacetime by::
+
+         w m / r
+
+Where m is the black hole mass, w (or omega) is the hole's angular
+velocity and r is the distance from the hole.
+
+This is known as the *Sciama Principle*, by Dennis Sciama, who proposed
+a theory of gravity based on the principle.
+
+The factor of 1/r is controversial, as the unique solution to
+Einstein's general relativity, in as vacuum gives the Kerr metric,
+which drops off by 1 / r^3.
+
+The solution to this conundrum, is that space is not a vacuum and so
+the Kerr metric does not apply.
+
+Now returning to nano-gravity.  The models that are being used assume
+the waves are from inspirals of super massive black holes, rather than
+just the direct rotations of the black holes themselves (see above re
+Kerr metric).
+
+Nonetheless, the waves are commensurate with the Schwartzchild radius
+of the black holes, with magnitude proportional to the mass and
+presumably dropping off with distance by a factor of 1/r.
+
+For a radius in years, we need 10^11 solar masses.
+
+Note also that the effect is dominated by distant galaxies, as is the
+CMB, since the number of galaxies at a given distance goes up with
+r^2.
+
+What about the high frequency waves LIGO is detecting?
+------------------------------------------------------
+
+These are the analogue of gamma-ray-bursts as describing in `gotu`.
+
+
+
+"""
