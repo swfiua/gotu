a6adfa34e697009373b10fc96231279bde0e9fdc
Jonny Gill <swfiua@gmail.com>
2026-03-11 15:39:44 -0400

    bilbo in numeric overflow land

--- gotu/bilbo.py
+++ gotu/bilbo.py
@@ -43,16 +43,112 @@ def time_domain_source_model(
 ):
     print('TDSM ******')
     print(len(gtimes), gtimes[0])
-    print('mas/theta/phi', mass, theta, phi)
+    print('mass/theta/phi', mass, theta, phi)
 
     galaxy = Spiral()
     galaxy.set_mcent((mass * 3.0 * u.km << u.lyr).value)
     galaxy.phi = phi
     galaxy.theta = theta
 
+    hubble_time = (galaxy.cosmo.cosmo.hubble_time << u.s).value
+
     print(geocent_time, gtimes[0], gtimes[-1], len(gtimes))
 
+    tstar = galaxy.tstar()
+
+    minz = -0.9999
+
+    phi_factor = phi
+    zeds = [1 - (1/(10**blue)) for blue in range(1, 8)]
+    while phi_factor < 35.:
+        galaxy.phi = phi_factor
+
+        tstar = galaxy.tstar()
+        tminz = find_t_forz(galaxy, hubble_time, minz)
+        tforz = [find_t_forz(galaxy, hubble_time, z) for z in zeds]
+        tb = galaxy.tb()
+        for zed, tforz in zip(zeds, tforz):
+            print('phi z t tb', phi_factor, zed, tforz-tstar, tb, galaxy.zandx(tstar+tb))
+
+
+        phi_factor *= 2.
+    # reset galaxy.phi
+    phi = galaxy.phi
+
+    inspiral = gtimes[:int(len(gtimes)/2)]
+
+    ttt = [tminz+((gtime-geocent_time)/hubble_time) for gtime in inspiral]
+    uuu = [galaxy.uoft(t) for t in ttt]
+    zandx = [galaxy.zandx(t, u) for t, u in zip(ttt, uuu)]
+    zzz = [zx[0] for zx in zandx]
+    xxx = [zx[1] * hubble_time for zx in zandx]
+
+    wavelength = (galaxy.schwartzchild() << u.lightsecond).value
+
+    delta_u = np.array(uuu[1:]) - np.array(uuu[:-1])
+    print('urange:', uuu[0], uuu[-1])
+    print('theta/phi/tb/tstar/umax', galaxy.theta, galaxy.phi, galaxy.tb(), galaxy.tstar(), galaxy.umax())
+
     return dict(foo=np.array([1e-18] * len(gtimes)))
+
+def find_t_forz(galaxy, hubble_time, z, epsilon=1e-6):
+
+    tstar = galaxy.tstar()
+    tmin = tstar
+    tb = galaxy.tb()
+    tmax = tb + tmin
+
+    while True:
+        t = (tmax+tmin)/2
+        zoft, xx = galaxy.zandx(t)
+        print(t-tstar, zoft)
+        if abs(zoft-z) < epsilon:
+            break
+        #print(tb, tmax, tmin, zoft, z)
+        if zoft > z:
+            if tmax == t:
+                print('tforz is tmax')
+                break
+            tmax = t
+        else:
+            if tmin == t:
+                print('tforz is tmin')
+                break
+            tmin = t
+
+        if tmax == tmin: break
+
+    return t
+
+async def tfortb_show(galaxy, mintheta=0.001, maxtheta=.8, minphi=.5, maxphi=5., tb_factor=0.001):
+
+    width = height = 1024
+    tc = magic.TableCounts(title=f'z for tb * {tb_factor}',
+        minx=mintheta, maxx=maxtheta, miny=minphi, maxy=maxphi,
+        width=width, height=height, xname='theta', yname='phi',
+        colorbar=True)
+    
+    tbtab = magic.TableCounts(title=f'blueshift time',
+        minx=mintheta, maxx=maxtheta, miny=minphi, maxy=maxphi,
+        width=width, height=height, xname='theta', yname='phi',
+        colorbar=True)
+    
+    thetas = np.linspace(mintheta, maxtheta, width)
+    phis = np.linspace(minphi, maxphi, height)
+
+    sp = Spiral()
+    for theta in thetas:
+        for phi in phis:
+            galaxy.theta = theta
+            galaxy.phi = phi
+            tb = galaxy.tb()
+            z, x = galaxy.zandx(galaxy.tstar() + galaxy.tb() * tb_factor)
+            tc.update([theta], [phi], 1/(1+z))
+            tbtab.update([theta], [phi], tb)
+
+    await tc.show()
+    await tbtab.show()
+
 
 class Sinh2(WeightedDiscreteValues):
 
